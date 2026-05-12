dd03bf7fecdf324d4c6f70eb415dd148f3c01622
demon4eg <d.kalugin@gmail.com>
2026-05-12 06:25:20 +0300

    works, but totaly mixed gui

--- main/ros_manager.c
+++ main/ros_manager.c
@@ -1,112 +1,228 @@
-#include <string.h>
+#include "ros_manager.h"
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/queue.h"
 #include "esp_log.h"
-#include "esp_system.h"
+#include "esp_timer.h"
+#include "esp_wifi.h"
 
-#include <uros_network_interfaces.h>
+#include "ui_manager.h"
+#include "display_config.h"
+
+// micro-ROS headers
 #include <rcl/rcl.h>
 #include <rcl/error_handling.h>
-#include <std_msgs/msg/int32.h>
 #include <rclc/rclc.h>
 #include <rclc/executor.h>
-
-#ifdef CONFIG_MICRO_ROS_ESP_XRCE_DDS_MIDDLEWARE
+#include <std_msgs/msg/int32.h>
+#include <std_msgs/msg/string.h>
+#include <manipulator_6dof_interfaces/msg/telemetry.h>
+#include <manipulator_6dof_interfaces/msg/terminal_cmd.h>
 #include <rmw_microros/rmw_microros.h>
-#endif
 
-#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Aborting.\n",__LINE__,(int)temp_rc);vTaskDelete(NULL);}}
-#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Continuing.\n",__LINE__,(int)temp_rc);}}
+// WiFi headers
+#include <uros_network_interfaces.h>
 
-rcl_publisher_t publisher_1;
-rcl_publisher_t publisher_2;
+static const char *TAG = "ROS_MGR";
 
-void thread_1(void * arg)
-{
-	std_msgs__msg__Int32 msg;
-	msg.data = 0;
-	while(1){
-		RCSOFTCHECK(rcl_publish(&publisher_1, &msg, NULL));
-		msg.data++;
-		usleep(1000000);
-	}
+typedef struct {
+    int type;
+    int id;
+    float value;
+} ros_cmd_t;
+
+static QueueHandle_t ros_cmd_queue;
+
+#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){ESP_LOGE(TAG, "Failed status on line %d: %d. Aborting.",__LINE__,(int)temp_rc); vTaskDelete(NULL);}}
+#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){ESP_LOGW(TAG, "Failed status on line %d: %d. Continuing.",__LINE__,(int)temp_rc);}}
+
+static rcl_publisher_t cmd_pub;
+static rcl_publisher_t hb_pub;
+static rcl_subscription_t telemetry_sub;
+static rcl_subscription_t debug_sub;
+
+static manipulator_6dof_interfaces__msg__TerminalCmd cmd_msg;
+static manipulator_6dof_interfaces__msg__Telemetry telemetry_msg;
+static std_msgs__msg__Int32 hb_msg;
+static std_msgs__msg__String debug_msg;
+
+static void timer_callback(rcl_timer_t * timer, int64_t last_call_time) {
+    if (timer != NULL) {
+        hb_msg.data = esp_timer_get_time() / 1000;
+        RCSOFTCHECK(rcl_publish(&hb_pub, &hb_msg, NULL));
+    }
 }
 
-void thread_2(void * arg)
+void debug_callback(const void * msgin)
 {
-	std_msgs__msg__Int32 msg;
-	msg.data = 0;
-	while(1){
-		RCSOFTCHECK(rcl_publish(&publisher_2, &msg, NULL));
-		msg.data--;
-		usleep(500000);
-	}
+    const std_msgs__msg__String * msg = (const std_msgs__msg__String *)msgin;
+
+    if (msg && msg->data.data) {
+        ui_add_debug_log(msg->data.data);
+    }
 }
 
-void micro_ros_task(void * arg)
+static void telemetry_callback(const void * msgin)
 {
-	rcl_allocator_t allocator = rcl_get_default_allocator();
-	rclc_support_t support;
+    const manipulator_6dof_interfaces__msg__Telemetry * msg =
+        (const manipulator_6dof_interfaces__msg__Telemetry *)msgin;
 
-	rcl_init_options_t init_options = rcl_get_zero_initialized_init_options();
-	RCCHECK(rcl_init_options_init(&init_options, allocator));
+    if (!msg) return;
+
+    float display_data[13];
+
+    for(int i = 0; i < 6; i++) {
+        display_data[i] = msg->xyzrpy[i];
+    }
+
+    for(int i = 0; i < 6; i++) {
+        display_data[i + 6] = msg->joints[i];
+    }
+
+    display_data[12] = msg->gripper;
+
+    ui_update_telemetry(
+        display_data, 13,
+        msg->state,
+        msg->tool_id,
+        msg->work_offset_id,
+        msg->cartesian_mode,
+        msg->speed_override
+    );
+}
+
+static void ros_ui_request_handler(int type, int id, float value)
+{
+    if (!ros_cmd_queue) return;
+
+    ros_cmd_t cmd = {
+        .type = type,
+        .id = id,
+        .value = value
+    };
+
+    xQueueSend(ros_cmd_queue, &cmd, 0);
+    ESP_LOGI(TAG, "CMD queued: type=%d id=%d val=%.2f", type, id, value);
+}
+
+void ros_send_full_state(void)
+{
+    cmd_msg.tool_id = ui_get_tool_id();
+    cmd_msg.work_offset_id = ui_get_work_offset_id();
+    cmd_msg.state = ui_get_robot_state();
+    cmd_msg.auto_state = ui_get_auto_state();
+    cmd_msg.cartesian_mode = ui_get_cartesian_mode();
+    cmd_msg.speed_override = ui_get_speed_override();
+    cmd_msg.estop = ui_get_estop_state();
+    cmd_msg.action_trigger = 0; // No action by default
+
+    RCSOFTCHECK(rcl_publish(&cmd_pub, &cmd_msg, NULL));
+
+    ESP_LOGI(TAG, "State sent: tool=%d work=%d state=%d auto_state=%d mode=%d estop=%d speed=%.2f",
+        cmd_msg.tool_id,
+        cmd_msg.work_offset_id,
+        cmd_msg.state,
+        cmd_msg.auto_state,
+        cmd_msg.cartesian_mode,
+        cmd_msg.estop,
+        cmd_msg.speed_override);
+}
+
+static void micro_ros_task(void * arg)
+{
+    rcl_allocator_t allocator = rcl_get_default_allocator();
+    rclc_support_t support;
+
+    rcl_init_options_t init_options = rcl_get_zero_initialized_init_options();
+    RCCHECK(rcl_init_options_init(&init_options, allocator));
 
 #ifdef CONFIG_MICRO_ROS_ESP_XRCE_DDS_MIDDLEWARE
-	rmw_init_options_t* rmw_options = rcl_init_options_get_rmw_init_options(&init_options);
-
-	// Static Agent IP and port can be used instead of autodisvery.
-	RCCHECK(rmw_uros_options_set_udp_address(CONFIG_MICRO_ROS_AGENT_IP, CONFIG_MICRO_ROS_AGENT_PORT, rmw_options));
-	//RCCHECK(rmw_uros_discover_agent(rmw_options));
+    rmw_init_options_t* rmw_options = rcl_init_options_get_rmw_init_options(&init_options);
+    RCCHECK(rmw_uros_options_set_udp_address(CONFIG_MICRO_ROS_AGENT_IP, CONFIG_MICRO_ROS_AGENT_PORT, rmw_options));
 #endif
 
-	// create init_options
-	RCCHECK(rclc_support_init_with_options(&support, 0, NULL, &init_options, &allocator));
+    RCCHECK(rclc_support_init_with_options(&support, 0, NULL, &init_options, &allocator));
 
-	// create node
-	rcl_node_t node;
-	RCCHECK(rclc_node_init_default(&node, "multithread_node", "", &support));
+    ui_register_command_callback(ros_ui_request_handler);
 
-	// create two publishers
-	RCCHECK(rclc_publisher_init_default(
-		&publisher_1,
-		&node,
-		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
-		"multithread_publisher_1"));
+    rcl_node_t node;
+    RCCHECK(rclc_node_init_default(&node, "esp32_gui_node", "", &support));
 
-	RCCHECK(rclc_publisher_init_default(
-		&publisher_2,
-		&node,
-		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
-		"multithread_publisher_2"));
+    RCCHECK(rclc_publisher_init_default(&hb_pub, &node, 
+        ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32), "terminal/heartbeat"));
+    RCCHECK(rclc_publisher_init_default(&cmd_pub, &node, 
+        ROSIDL_GET_MSG_TYPE_SUPPORT(manipulator_6dof_interfaces, msg, TerminalCmd), "terminal/command"));
 
+    RCCHECK(rclc_subscription_init_default(&debug_sub, &node, 
+        ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, String), "robot/debug"));
+    RCCHECK(rclc_subscription_init_default(&telemetry_sub, &node, 
+        ROSIDL_GET_MSG_TYPE_SUPPORT(manipulator_6dof_interfaces, msg, Telemetry), "robot/telemetry"));
 
-	xTaskCreate(thread_1,
-		"thread_1",
-		CONFIG_MICRO_ROS_APP_STACK,
-		NULL,
-		CONFIG_MICRO_ROS_APP_TASK_PRIO,
-		NULL);
+    ros_cmd_queue = xQueueCreate(10, sizeof(ros_cmd_t));
 
-	xTaskCreate(thread_2,
-		"thread_2",
-		CONFIG_MICRO_ROS_APP_STACK,
-		NULL,
-		CONFIG_MICRO_ROS_APP_TASK_PRIO + 1,
-		NULL);
+    rcl_timer_t timer;
+    const unsigned int timer_timeout = 1000;
+    RCCHECK(rclc_timer_init_default2(&timer, &support, RCL_MS_TO_NS(timer_timeout), timer_callback, true));
 
-	while(1){
-		sleep(100);
-	}
+    rclc_executor_t executor;
+    RCCHECK(rclc_executor_init(&executor, &support.context, 4, &allocator));
+    RCCHECK(rclc_executor_add_timer(&executor, &timer));
+    RCCHECK(rclc_executor_add_subscription(&executor, &debug_sub, &debug_msg, &debug_callback, ON_NEW_DATA));
+    RCCHECK(rclc_executor_add_subscription(&executor, &telemetry_sub, &telemetry_msg, &telemetry_callback, ON_NEW_DATA));
 
-	// free resources
-	RCCHECK(rcl_publisher_fini(&publisher_1, &node));
-	RCCHECK(rcl_publisher_fini(&publisher_2, &node));
-	RCCHECK(rcl_node_fini(&node));
+    std_msgs__msg__String__init(&debug_msg);
+    debug_msg.data.data = (char*)malloc(256);
+    debug_msg.data.capacity = 256;
+    debug_msg.data.size = 0;
 
-  	vTaskDelete(NULL);
+    manipulator_6dof_interfaces__msg__TerminalCmd__init(&cmd_msg);
+    manipulator_6dof_interfaces__msg__Telemetry__init(&telemetry_msg);
+    std_msgs__msg__Int32__init(&hb_msg);
+
+    ESP_LOGI(TAG, "micro-ROS WiFi node started");
+
+    vTaskDelay(pdMS_TO_TICKS(1000));
+    ros_send_full_state();
+
+    while(1) {
+        ros_cmd_t cmd;
+        while (xQueueReceive(ros_cmd_queue, &cmd, 0) == pdTRUE) {
+            switch(cmd.type) {
+                case 1: cmd_msg.work_offset_id = (uint8_t)cmd.id; break;
+                case 2: cmd_msg.tool_id = (uint8_t)cmd.id; break;
+                case 3: cmd_msg.state = (uint8_t)cmd.id; break;
+                case 4: cmd_msg.auto_state = (uint8_t)cmd.id; break;
+                case 5: cmd_msg.cartesian_mode = (cmd.id == 1); break;
+                case 6: cmd_msg.speed_override = cmd.value; break;
+                case 7: cmd_msg.estop = (cmd.id == 1); break;
+                case 8: cmd_msg.action_trigger = (uint8_t)cmd.id; break;
+            }
+
+            RCSOFTCHECK(rcl_publish(&cmd_pub, &cmd_msg, NULL));
+
+            ESP_LOGI(TAG, "CMD sent: tool=%d work=%d state=%d auto_state=%d mode=%d estop=%d speed=%.2f trigger=%d",
+                    cmd_msg.tool_id,
+                    cmd_msg.work_offset_id,
+                    cmd_msg.state,
+                    cmd_msg.auto_state,
+                    cmd_msg.cartesian_mode,
+                    cmd_msg.estop,
+                    cmd_msg.speed_override,
+                    cmd_msg.action_trigger);
+            
+            if(cmd.type == 8) cmd_msg.action_trigger = 0;
+        }
+
+        rclc_executor_spin_some(&executor, RCL_MS_TO_NS(10));
+        vTaskDelay(pdMS_TO_TICKS(10));
+    }
+
+    free(debug_msg.data.data);
+    vTaskDelete(NULL);
 }
 
 void ros_manager_init(void)
@@ -115,7 +231,9 @@ void ros_manager_init(void)
     ESP_ERROR_CHECK(uros_network_interface_initialize());
 #endif
 
-    //pin micro-ros task in APP_CPU to make PRO_CPU to deal with wifi:
+    esp_wifi_set_ps(WIFI_PS_NONE);
+    ESP_LOGI(TAG, "WiFi power saving disabled");
+
     xTaskCreate(micro_ros_task,
             "uros_task",
             CONFIG_MICRO_ROS_APP_STACK,
--- main/ui_manager.h
+++ main/ui_manager.h
@@ -1,16 +1,56 @@
 #ifndef UI_MANAGER_H
 #define UI_MANAGER_H
 
-#include <esp_err.h>
-#include <lvgl.h>
+#include "esp_err.h"
+#include <stdbool.h>
+#include <stdint.h>
 
+// Типы команд для ROS
+typedef enum {
+    CMD_TYPE_WORK_OFFSET = 1,
+    CMD_TYPE_TOOL_ID = 2,
+    CMD_TYPE_STATE = 3,
+    CMD_TYPE_AUTO_STATE = 4,
+    CMD_TYPE_CARTESIAN_MODE = 5,
+    CMD_TYPE_SPEED_OVERRIDE = 6,
+    CMD_TYPE_ESTOP = 7,
+    CMD_TYPE_ACTION_TRIGGER = 8
+} ui_command_type_t;
+
+// Колбэк для отправки команд в ROS
+typedef void (*ui_command_callback_t)(int type, int id, float value);
+
+// Инициализация UI
 esp_err_t ui_init(void);
 void ui_task_handler(void);
-void ui_create_demo(void);  // Keep old demo for testing
-void ui_create_robot_control(void);  // New robot control UI
+
+// Создание UI интерфейса
+void ui_create_robot_control(void);
+void ui_create_demo(void);
 void ui_clear_screen(void);
 
-// Widget access functions
+// Функции для обновления UI данными из ROS
+void ui_update_telemetry(float *values, int count, uint8_t state, 
+                        uint8_t tool_id, uint8_t work_offset_id, 
+                        bool cartesian_mode, float speed_override);
+void ui_add_debug_log(const char *message);
+
+// Функции для получения текущего состояния UI
+uint8_t ui_get_tool_id(void);
+uint8_t ui_get_work_offset_id(void);
+uint8_t ui_get_robot_state(void);
+uint8_t ui_get_auto_state(void);
+bool ui_get_cartesian_mode(void);
+float ui_get_speed_override(void);
+bool ui_get_estop_state(void);
+
+// Регистрация колбэка для команд
+void ui_register_command_callback(ui_command_callback_t callback);
+
+// Отправка команды из UI
+void ui_send_command(ui_command_type_t type, int id, float value);
+
+// Старые функции для обратной совместимости
 void ui_set_terminal_text(const char *text);
 void ui_update_state_values(float *values, int count);
 void ui_set_work_offset(int index);
